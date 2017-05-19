0e9629b360293883577c3ef5ae26a9b2c792fbc8
Tony <alex@orangesky.org.au>
2017-05-19 12:27:30 +1000

    Styling InfoWindow

--- server/display/script.js
+++ server/display/script.js
@@ -55,7 +55,7 @@ function createMap(trackData) {
 			lng: marker.longitude,
 			title: markerDate.toString(),
 			infoWindow: {
-				content: '<div style="height:360px;"><img class="rotateimg90 fillheight" src="' + marker.photo_url + '"><br><span>' + marker.text + '</span></div>'
+				content: '<div style="height:360px;"><img class="rotateimg90" width="200px" src="' + marker.photo_url + '"><br><span>' + marker.text + '</span></div>'
 			}
 		});
 		path.push([marker.latitude, marker.longitude]);
