627110694dfc8bfcc83c79292eba95efd1d819a0
Rhys May <rhys@orangeskylaundry.com.au>
2017-05-19 11:36:30 +1000

    Added another 0

--- server/display/script.js
+++ server/display/script.js
@@ -1,7 +1,7 @@
 // Start getting map data on document ready
 $(document).ready(function() {
     var trackData = getTrackData();
-    window.setInterval(location.reload(true), 100000);
+    window.setInterval(location.reload(true), 1000000);
 });
 
 // Gets track data from file and returns array
